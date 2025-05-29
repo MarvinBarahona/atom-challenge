86849dfd4ee12d1a984a0d3905be2f74a765f7dd
Marvin Barahona <marosvramirez@gmail.com>
2025-05-29 16:14:29 -0600

    chore: update eslint version

---
+++ functions/eslint.config.js
@@ -0,0 +1,61 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const globals = require("globals");
+
+const {
+    fixupConfigRules,
+    fixupPluginRules,
+} = require("@eslint/compat");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        sourceType: "module",
+
+        parserOptions: {
+            project: ["tsconfig.json", "tsconfig.dev.json"],
+        },
+    },
+
+    ignores: ["eslint.config.js"],
+
+    extends: fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended",
+    )),
+
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    rules: {
+        "quotes": ["error", "double"],
+        "import/no-unresolved": 0,
+        "indent": ["error", 2],
+        "max-len": ["error", 100],
+    },
+}, globalIgnores(["lib/**/*", "generated/**/*"])]);
