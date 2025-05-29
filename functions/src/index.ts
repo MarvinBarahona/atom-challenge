1bbe3020c460c139113bcfd715fba04c0e231dd0
Marvin Barahona <marosvramirez@gmail.com>
2025-05-28 21:08:18 -0600

    chore: test GitHub action

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,4 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
@@ -15,5 +6,5 @@ import * as logger from "firebase-functions/logger";
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase! Edited to test Github Action. Test 1");
 });
