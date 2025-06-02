605b84454578b9492988098b53c0ee21d7ee3dc2
Marvin Barahona <marosvramirez@gmail.com>
2025-06-02 16:03:05 -0600

    feat: persist userId in localStorage

--- app/src/app/shared/auth.service.ts
+++ app/src/app/shared/auth.service.ts
@@ -6,12 +6,18 @@ import { Injectable } from '@angular/core';
 export class AuthService {
     userId?: string;
 
+    constructor() {
+        this.userId = localStorage.getItem("userId") ?? undefined;
+    }
+
     login(userId: string): void {
         this.userId = userId;
+        localStorage.setItem("userId", this.userId);
     }
 
     logout(): void {
         this.userId = undefined;
+        localStorage.removeItem("userId");
     }
 
     loggedIn(): boolean {
