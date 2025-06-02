1bce308eeab1e425f5a8cb92cdbcd5d30946dd36
Marvin Barahona <marosvramirez@gmail.com>
2025-06-02 16:02:50 -0600

    feat: add redirection in guards

--- app/src/app/routing/guards/logged-in.guard.ts
+++ app/src/app/routing/guards/logged-in.guard.ts
@@ -1,8 +1,10 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../../shared/auth.service';
 
 export const loggedInGuard: CanActivateFn = () => {
     const authService = inject(AuthService);
-    return authService.loggedIn();
+    const router = inject(Router);
+
+    return authService.loggedIn() || router.createUrlTree(['/login']);
 };
